561b49a19829f91633aa4718fbce9e3c9978254b
fuji <fujiwarahidenari@gmail.com>
2021-02-15 17:53:39 +0900

    #39 HOME：PCメニュー 固定表示へ変更

--- static/static/js/common.js
+++ static/static/js/common.js
@@ -108,22 +108,7 @@ $(function(){
   }
 
   // menu スクロール表示
-  if(isPcSize){
-    var $win = $(window),
-        $header = $('.header'),
-        headerHeight = $header.outerHeight(),
-        startPos = 0;
-
-    $win.on('load scroll', function() {
-      var value = $(this).scrollTop();
-      if ( value > startPos && value > headerHeight ) {
-        $header.css('top', '-' + headerHeight + 'px');
-      } else {
-        $header.css('top', '0');
-      }
-      startPos = value;
-    });
-  } else if(isMobileSize){
+  if(isMobileSize){
     var $win = $(window),
         $header = $('.contents-header'),
         headerHeight = $header.outerHeight(),
