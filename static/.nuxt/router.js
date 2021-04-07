fc94b0a673875e2e4f63d1cd71e93209b19a9550
fuji <fujiwarahidenari@gmail.com>
2021-04-07 17:30:02 +0900

    #66 3/11 記事削除、4/7 記事作成

--- static/.nuxt/router.js
+++ static/.nuxt/router.js
@@ -12,6 +12,7 @@ const _50a8bf5c = () => interopDefault(import('../pages/news/202101/index.vue' /
 const _712b4306 = () => interopDefault(import('../pages/news/202101/30/index.vue' /* webpackChunkName: "pages/news/202101/30/index" */))
 const _8b2f8372 = () => interopDefault(import('../pages/news/202103/11/index.vue' /* webpackChunkName: "pages/news/202103/11/index" */))
 const _34e6d644 = () => interopDefault(import('../pages/news/202103/30/index.vue' /* webpackChunkName: "pages/news/202103/30/index" */))
+const _11f0930d = () => interopDefault(import('../pages/news/202104/07/index.vue' /* webpackChunkName: "pages/news/202104/07/index" */))
 const _d388099e = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
@@ -62,6 +63,10 @@ export const routerOptions = {
     path: "/news/202103/30",
     component: _34e6d644,
     name: "news-202103-30"
+  }, {
+    path: "/news/202104/07",
+    component: _11f0930d,
+    name: "news-202104-07"
   }, {
     path: "/",
     component: _d388099e,
